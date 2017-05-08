810cd8c3ffca0f498b09db93177e1817ffd2479b
jeisson <jeisson@Jeisson>
2017-05-08 10:13:28 -0500

    juego

---
+++ src/zombie1/TestPaintComponent.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Zombie1;
+
+/**
+ *
+ * @author jeisson
+ */
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author Estudiante
+ */
+public class TestPaintComponent extends JFrame{
+    public TestPaintComponent(){
+        add(new Nivel1());
+    }
+}
